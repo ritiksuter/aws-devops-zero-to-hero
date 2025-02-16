705f27de12bfe37ae8ced7b62f4025bd87778d88
Ritik Suteri <141510124+ritiksuter@users.noreply.github.com>
2025-02-17 01:01:11 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # Returning string from this function (Here we have a simple flask application)
+    # Returning string...
     return 'Hello, world!'
 
 if __name__ == '__main__':
